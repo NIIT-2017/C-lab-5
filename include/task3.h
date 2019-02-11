e45195d9577a7860af923620c60487598be67113
ipetrovanton <ipetrovanton@gmail.com>
2019-02-11 20:59:52 +0300

    удлил макросы из заголовочного файла

--- include/task3.h
+++ include/task3.h
@@ -1,14 +1,5 @@
 
-#ifndef task3_h
-#define task3_h
-
-#define N 20
-#define M 256
-
 #include <stdio.h>
 
 char *mixChars(char *in, char *out);
 char *mixLine(char *instr, char * outstr);
-
-
-#endif /* task3_hpp */
