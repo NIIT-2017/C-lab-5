02b33f370afdafa80249dead5836535f8f96ad2c
Shalagin Nikolay <qquikly@mail.ru>
2019-02-25 20:02:14 +0300

    add includes

---
+++ include/task2.h
@@ -0,0 +1,7 @@
+
+#define M 6 // size matrix
+
+void clearMatrix(char(*arr)[M]);
+void fillMatrix(char(*arr)[M]);
+void setMatrix(char(*arr)[M]);
+void printMatrix(char(*arr)[M]);
---
+++ include/task3.h
@@ -0,0 +1,3 @@
+char *mixChars(char *in, char *out);// -перемешивание символов в одном слове
+char *mixLine(char *instr, char * outstr);// -перемешивание для целой строки
+void CleanMass(char *in);// -очистка массива
