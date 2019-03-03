a4e3fadb495414c350611f05979a6d6436743795
Nikolai <44969889+nbuzynin@users.noreply.github.com>
2019-03-03 22:29:38 +0300

    Add files via upload

--- include/task3.h
+++ include/task3.h
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <stdlib.h>
 #include <time.h>
-#define WORDS 256
+#define WORDS 512
 
 char *mixChars(char *in, char *out);						//Перемешивание символов в одном слове
 char *mixLine(char *instr, char * outstr);					//Перемешивание для целой строки
