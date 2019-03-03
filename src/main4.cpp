9d6d0139e1ff44f53cb6f14cd937ce9df0a8f2c6
sergone <medov777@yandex.ru>
2019-03-03 18:54:46 +0300

    B9 Lab 5

---
+++ src/main4.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+#include "task1.h"
+#include <time.h>
+#include <stdlib.h>
+#define sLMax 256 
+
+FILE *mf1;
+FILE *mf2;
+
+int main()
+{
+	char in[sLMax] = { 0 };
+	char out[sLMax];
+	srand(time(0));
+	mf1 = fopen("task4.txt", "r");
+	if (mf1 == NULL) 
+	{
+		printf("file open error\n");
+		return -1;
+	}
+	mf2 = fopen("task4-2.txt", "w");
+	if (mf2 == NULL) 
+	{
+		printf("file open error\n");
+		return -2;
+	}
+
+	while (fgets(in, sLMax, mf1) != NULL)
+	{
+		if (in[strlen(in) - 1] == '\n')
+			in[strlen(in) - 1] = '\0';
+		fputs(randomWords(in, out), mf2);
+		fputs("\n", mf2);
+	}
+	fclose(mf1);
+	fclose(mf2);
+	return 0;
+}
---
+++ src/task3.cpp
@@ -0,0 +1,37 @@
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#define COUNT_WORDS_MAX 20
+#define LENGTH_WORDS_MAX 30
+
+char *mixChars(char *in, char *out)
+{
+	int len = 0; // length of word
+	while (*(in + len) != ' ' && *(in + len) != '\n' && *(in + len) != '\0')
+	{
+		*(out + len) = *(in + len);
+		len++;
+	}
+	char temp = 0;
+	int randNum = 0;
+	for (int j = 1; j < len - 1; j++)
+	{
+		temp = *(out + j);
+		randNum = j + rand() % (len - 1 - j);
+		*(out + j) = *(out + randNum);
+		*(out + randNum) = temp;
+	}
+	return out;
+}
+
+char *mixLine(char *instr, char * outstr)
+{
+	for (int i = 0; i <= strlen(instr); i++)
+	{
+		if (*(instr + i) != ' ' && *(instr + i) != '\0' && *(instr + i) != '\n' && (i == 0 || *(instr + i - 1) == ' '))
+			mixChars(instr + i, outstr + i);
+		else if (*(instr + i) == ' ' || *(instr + i) == '\0' || *(instr + i) == '\n')
+			*(outstr + i) = *(instr + i);
+	}
+	return outstr;
+}
