85036fe9b96c31b4c4414e833922cb01c802e9f4
EveerDrof <EveerDrof@git.com>
2020-01-21 17:01:12 +0300

    Copied readFile in main4

--- src/main4.cpp
+++ src/main4.cpp
@@ -1,11 +1,38 @@
 #include "task1.h"
-#include "task3.h"
+char** readfile(FILE* file, int* linesNumber)
+{
+    int count = 0;
+    char buf;
+    while (fread(&buf, sizeof(char), 1, file))
+    {
+        if (buf == '\n')
+            count++;
+    }
+    *linesNumber = count;
+    char** lines = (char**)malloc(count * sizeof(char*));
+    rewind(file);
+    for (int i = 0; i < count; i++)
+    {
+        int lineLen = 0;
+        do
+        {
+            fread(&buf, sizeof(char), 1, file);
+            lineLen++;
+        } while (buf != '\0' && buf != '\n');
+        lineLen++;
+        lines[i] = (char*)malloc(lineLen * sizeof(char));
+        fseek(file, ftell(file) - lineLen, SEEK_SET);
+        fgets(lines[i], 100, file);
+    }
+    return lines;
+}
+
 int main()
 {
     srand(time(NULL));
     FILE* fileIn = fopen("Data.txt", "r");
     int linesNumber;
-    char** lines = readFile(fileIn, &linesNumber);
+    char** lines = readfile(fileIn, &linesNumber);
     for (int i = 0; i < linesNumber; i++)
     {
         char* buf = (char*)malloc(sizeof(char) * strlen(lines[i]));
